22c6b7e8d1b229cdc2dbca16574e303f7f6ea6f8
rkukin <romakukin@gmail.com>
2019-08-20 19:19:11 +0300

    task4 done

--- js/task4.js
+++ js/task4.js
@@ -1,1 +1,34 @@
 "use strict";
+
+class StringBuilder {
+  constructor(value) {
+    this._value = value;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  append(symbol) {
+    this._value = this._value + symbol;
+  }
+
+  prepend(symbol) {
+    this._value = symbol + this._value;
+  }
+
+  pad(symbol) {
+    this._value = symbol + this._value + symbol;
+  }
+}
+
+const builder = new StringBuilder(".");
+
+builder.append("^");
+console.log(builder.value); // '.^'
+
+builder.prepend("^");
+console.log(builder.value); // '^.^'
+
+builder.pad("=");
+console.log(builder.value); // '=^.^='
