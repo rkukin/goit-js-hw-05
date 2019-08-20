3b3dab28aae05a257255b135c1e330100c9c3221
rkukin <romakukin@gmail.com>
2019-08-20 18:15:27 +0300

    task1 done

--- js/task1.js
+++ js/task1.js
@@ -8,3 +8,9 @@ const Account = function(login, email) {
 Account.prototype.getInfo = function() {
   console.log(`Login: ${this.login}, Email: ${this.email}`);
 };
+
+const mango = new Account("Mangozedog", "mango@dog.woof");
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+const poly = new Account("Poly", "poly@mail.com");
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
